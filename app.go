66b488a8513adc6966cb04d8ec488224205de014
Elvin Abaev <elvinabaev@gmail.com>
2023-09-06 16:47:09 +0300

    Meeting with pointers and structures.

--- app.go
+++ app.go
@@ -2,31 +2,52 @@ package main
 
 import "fmt"
 
+type Person struct {
+	NAME   string
+	AGE    int
+	SEX    string
+	SALARY int
+}
+
+type Car struct {
+	MODEL              string
+	NEW                bool
+	YEAR_OF_PRODUCTION int
+	PRICE              int
+}
+
 func main() {
-	for i := 0; i < 100; i++ {
-		if i == 10 {
-			break
-		}
-		fmt.Println(i)
+	//a := "hello"
+	//b := 12
+	//
+	//c := &a
+	//d := &b
+	//
+	//fmt.Println(c)
+	//fmt.Println(d)
+	//fmt.Println(*c)
+	//fmt.Println(*d)
+
+	workingWithCar()
+}
+
+func workingWithPerson() {
+	myPerson := Person{
+		NAME:   "Elvin",
+		AGE:    22,
+		SEX:    "M",
+		SALARY: 1000,
 	}
+	fmt.Println("Name of person: " + myPerson.NAME)
+	fmt.Println(fmt.Sprintf("Age of person: %d", myPerson.AGE))
+	fmt.Println(fmt.Sprintf("Salary of person: %d", myPerson.SALARY))
+}
 
-	fmt.Println("-------------------------------------------------")
+func workingWithCar() {
+	car := Car{"Bugatti Veyron", true, 2019, 24559}
+	getCarInfo(car)
+}
 
-	b := 10
-	switch b {
-	case 1:
-		fmt.Println("1")
-
-	case 2:
-		fmt.Println("2")
-
-	default:
-		fmt.Println("Not 1 and 2")
-	}
-
-	fmt.Println("-------------------------------------------------")
-
-	defer fmt.Println("Hello!")
-	fmt.Println("Goodbye!")
-
+func getCarInfo(car Car) {
+	fmt.Println(fmt.Sprintf("Model: %s, new: %t, year: %d, price: %d", car.MODEL, car.NEW, car.YEAR_OF_PRODUCTION, car.PRICE))
 }
