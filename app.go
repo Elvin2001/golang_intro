f588f202cb68ef7cbcf8ebd98d7a83d239a360da
Elvin Abaev <elvinabaev@gmail.com>
2023-09-07 02:20:56 +0300

    Meeting with arrays, slices. Working with structures and methods.

--- app.go
+++ app.go
@@ -9,6 +9,20 @@ type Person struct {
 	SALARY int
 }
 
+type Book struct {
+	TITLE              string
+	AUTHOR             string
+	COUNT_OF_LISTS     int
+	YEAR_OF_PRODUCTION int
+}
+
+type Author struct {
+	NAME string
+	AGE  int
+	SEX  string
+	BOOK Book
+}
+
 type Car struct {
 	MODEL              string
 	NEW                bool
@@ -17,18 +31,70 @@ type Car struct {
 }
 
 func main() {
-	//a := "hello"
-	//b := 12
-	//
-	//c := &a
-	//d := &b
-	//
-	//fmt.Println(c)
-	//fmt.Println(d)
-	//fmt.Println(*c)
-	//fmt.Println(*d)
 
+	// arrays
+	var arr [4]string
+	arr[0] = "Elvin"
+	arr[1] = "Eduard"
+	arr[2] = "Evgeniy"
+	arr[3] = "Sonya"
+
+	elements := arr[0:2]
+	fmt.Println(elements)
+	fmt.Println(arr)
+
+	strings := []string{"el", "wl", "wq", "xl", "al", "gl"}
+
+	if strings == nil {
+		fmt.Println("Nil")
+	} else {
+		fmt.Println("Not nil")
+	}
+	str := strings[:]
+	fmt.Println(str)
+
+	names := [...]string{"Elvin", "Evgeniy", "Denis", "Alexander", "Vladimir", "Sofia", "Polina"}
+
+	for i := 0; i < len(names); i++ {
+		fmt.Println("Current elem: " + names[i])
+	}
+
+	// slices
+	colors := []string{
+		"green",
+		"yellow",
+		"black",
+		"blue",
+		"white",
+	}
+
+	colors = append(colors, "violet")
+
+	for i := range colors {
+		fmt.Println(colors[i])
+	}
+
+	createSlice := make([]string, 3)
+	createSlice[0] = "a"
+	createSlice[1] = "b"
+	createSlice[2] = "c"
+	a := "hello"
+	b := 12
+
+	c := &a
+	d := &b
+
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(*c)
+	fmt.Println(*d)
+	book := Book{"The green mile", "Steven King", 245, 2004}
+	author := Author{"Steven King", 57, "M", book}
+	fmt.Println(book.TITLE)
+	fmt.Println(book.AUTHOR)
+	book.method()
 	workingWithCar()
+	fmt.Println(author)
 }
 
 func workingWithPerson() {
@@ -50,4 +116,9 @@ func workingWithCar() {
 
 func getCarInfo(car Car) {
 	fmt.Println(fmt.Sprintf("Model: %s, new: %t, year: %d, price: %d", car.MODEL, car.NEW, car.YEAR_OF_PRODUCTION, car.PRICE))
+}
+
+func (book Book) method() {
+	fmt.Println(book.TITLE)
+	fmt.Println(book.AUTHOR)
 }
